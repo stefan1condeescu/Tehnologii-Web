f77d94794b6ddfabdfd16d7c6d32a6da40e9eaa4
Stefan <stefan@Laptop-Stefan.local>
2025-10-08 17:21:56 +0300

    add .gitignore, package.json and update index.js

--- seminar1/index.js
+++ seminar1/index.js
@@ -1,1 +1,14 @@
-require('./hello');
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res) => {
+    res.send('hello world')
+})
+
+app.get('/ping', (req, res) => {
+    res.send('pong')
+})
+  
+
+app.listen(8080)
