3bb4730e2fc0c1288f8e6cd242edebf94bda35c2
Stefan <stefan@Laptop-Stefan.local>
2025-11-27 15:32:29 +0200

    export functie si constanta

---
+++ EvenimenteSiDOM/main.js
@@ -0,0 +1,5 @@
+import { PI, add } from './math.js';
+
+console.log('Valoarea lui PI este:', PI);
+console.log('1 + 2 =', add(1, 2));
+console.log('10 + 5 =', add(10, 5));
---
+++ EvenimenteSiDOM/math.js
@@ -0,0 +1,6 @@
+
+export const PI = 3.1415;
+
+export function add(a, b) {
+  return a + b;
+}
